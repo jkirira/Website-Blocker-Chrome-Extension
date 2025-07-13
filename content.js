c847757e79dd77cba1db355821c1858365a2121e
jkirira <jkirira@cytonn.com>
2025-07-13 14:56:29 +0300

    Add day, start time and end time fields.

--- content.js
+++ content.js
@@ -1,11 +1,32 @@
 chrome.storage.sync.get("blockedSites", function(data) {
   var blockedSites = data.blockedSites;
 
-  for (var i = 0; i < blockedSites.length; i++) {
-    if (window.location.href.includes(blockedSites[i])) {
-      
-      // empty the content of the page
-      document.documentElement.innerHTML = "";
-    }
+  if (!blockedSites || !Array.isArray(blockedSites)) {
+    console.info('No blocked sites found.');
+    return;
+  }
+
+  const Today = new Date();
+
+  var current_day = Today.getDay()
+  var formatted_current_time = `${Today.getHours()}.${Today.getMinutes()}`;
+
+  var shouldBlock = blockedSites.some(site => {
+    var formatted_start_time = site.start.replace(":", ".")
+    var formatted_end_time = site.end.replace(":", ".")
+
+    return window.location.href.includes(site.url) &&
+            (current_day == site.day || site.day == "everyday") &&
+            parseFloat(formatted_current_time) > parseFloat(formatted_start_time) &&
+            parseFloat(formatted_current_time) < parseFloat(formatted_end_time)
+  })
+
+  if (shouldBlock) {
+    // empty the content of the page
+    document.documentElement.innerHTML = "Blocked.";
   }
 });
+
+// TODO:
+//  - edit
+//  - checkbox days
--- popup.js
+++ popup.js
@@ -2,14 +2,34 @@ document.addEventListener("DOMContentLoaded", function () {
   var addSiteButton = document.getElementById("addSite");
   addSiteButton.addEventListener("click", function () {
     var siteInput = document.getElementById("siteInput").value;
+    var day = document.getElementById("day").value;
+    var block_start_time = document.getElementById("block_start_time").value;
+    var block_end_time = document.getElementById("block_end_time").value;
+
     if (siteInput) {
       chrome.storage.sync.get("blockedSites", function (data) {
         var blockedSites = data.blockedSites || [];
-        blockedSites.push(siteInput);
+        var last_added_id = blockedSites.reduce(
+          (accumulator, currentValue) => accumulator > currentValue ? accumulator : currentValue,
+          0,
+        );
+
+        blockedSites.push({
+          id : last_added_id + 1,
+          url: siteInput,
+          day: day,
+          start: block_start_time,
+          end: block_end_time
+        });
+
         chrome.storage.sync.set({ blockedSites: blockedSites });
         updateBlockedList();
       });
+
       document.getElementById("siteInput").value = "";
+      document.getElementById("day").value = "everyday";
+      document.getElementById("block_start_time").value = "00:00";
+      document.getElementById("block_end_time").value = "23:59";
     }
   });
 
@@ -23,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
       blockedList.innerHTML = "";
       blockedSites.forEach(function (site) {
         var li = document.createElement("li");
-        li.textContent = site;
+        li.textContent = site.url;
 
         // add a remove button
 
@@ -33,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
         removeButton.addEventListener("click", function () {
           chrome.storage.sync.get("blockedSites", function (data) {
             var blockedSites = data.blockedSites;
-            var index = blockedSites.indexOf(site);
+            var index = blockedSites.findIndex(blockedSite => blockedSite.id == site.id);
             if (index !== -1) {
               blockedSites.splice(index, 1);
               chrome.storage.sync.set({ blockedSites: blockedSites });
